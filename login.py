368c4c56654d62d41b1a19921393fc8d679ea085
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2016-12-21 18:09:12 +0100

    Correzione

--- login.py
+++ login.py
@@ -1,4 +1,42 @@
-se("Hello World!")
+from flask import Flask, Response, redirect, url_for, request, session, abort
+from flask.ext.login import LoginManager, UserMixin, \
+                                login_required, login_user, logout_user 
+
+app = Flask(__name__)
+
+# config
+app.config.update(
+    DEBUG = True,
+    SECRET_KEY = 'secret_xxx'
+)
+
+# flask-login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+# silly user model
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_secret"
+        
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+# create some users with ids 1 to 20       
+users = [User(id) for id in range(1, 21)]
+
+
+# some protected url
+@app.route('/')
+@login_required
+def home():
+    return Response("Hello World!")
 
  
 # somewhere to login
